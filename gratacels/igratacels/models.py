273987f46a6111b9e20241a39de32d2cae6638d9
Saida <saida@saida.(none)>
2013-03-26 12:09:50 +0100

    Creació del model Arquitectes

--- gratacels/igratacels/models.py
+++ gratacels/igratacels/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Arquitecte (models.Model):
+	idArquitecte = models.IntegerField(primary_key=True)
+	nom = models.TextField(max_length=25)
+	cognom = models.TextField(max_length=25)
+	data_naixement = models.DateTimeField()
+	nacionalitat = models.TextField(max_length=25)
